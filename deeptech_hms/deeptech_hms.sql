a0e9a46b752ed3de28f95d908c6eccfae2b48ee1
Ajibola, A. Omolabake <ajibolaaishat5@gmail.com>
2026-03-21 16:25:29 +0100

    Create deeptech_hms.sql

---
+++ deeptech_hms/deeptech_hms.sql
@@ -0,0 +1,18 @@
+-- Identifying the database to query
+USE deeptech_hms_db;
+
+-- Identifying Patients in FCT and Plateau
+SELECT * FROM patient_table
+WHERE State IN ('Fct Abuja', 'Plateau');
+
+-- Retrieving the total number of male and female patients
+SELECT Gender, COUNT(*) AS Total
+FROM patient_table
+GROUP BY Gender;
+
+-- List of doctors and their specialities in state where confirmed appointment exist
+SELECT a.AppointmentID, a.PatientID, a.Date, a.Status, d.DoctorID, d.Name, d.Speciality, d.State
+FROM appointment_table a 
+JOIN doctor_table d 
+WHERE a.Status = 'Confirmed'
+GROUP BY a.AppointmentID, a.PatientID, d.DoctorID;
